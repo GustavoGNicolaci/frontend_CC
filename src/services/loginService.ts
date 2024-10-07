9a124fcfddeee1b61b89d0747be4607b0ca61cf6
GeoFernandes <geovannaferoliveira@gmail.com>
2024-10-07 20:31:52 -0300

    Correção do objeto e login

--- src/services/loginService.ts
+++ src/services/loginService.ts
@@ -1,9 +1,9 @@
 // src/services/authService.js
 import axios from 'axios';
 
-export const login = async (email, password) => {
+export const login = async (email, senha) => {
     try {
-        const response = await axios.post('http://localhost:8080/auth/login', { email, password });
+        const response = await axios.post('http://localhost:8080/auth/login', { email, senha });
         return response.data;
     } catch (error) {
         throw new Error('Erro ao fazer login');
