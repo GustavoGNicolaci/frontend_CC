fe26b31b4f44c4530b90ab5b8a60f68e858bbb00
GeoFernandes <geovannaferoliveira@gmail.com>
2024-10-24 18:49:17 -0300

    Inclusão de modal para trocar de senha

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,20 +1,21 @@
-import React, { useState } from 'react'; // Importando useState
+import React, { useState } from 'react';
 import { login } from '../../services/loginService';
-import { useNavigate } from 'react-router-dom'; 
-import Footer from '../footer'; // Importando o componente Footer
-import NavbarComponent from '../navbar'; // Importando o componente Navbar
-import styles from './login.module.css'; // Importando o CSS Module
+import { useNavigate } from 'react-router-dom';
+import Footer from '../footer';
+import NavbarComponent from '../navbar';
+import styles from './login.module.css';
 
 const Login = () => {
-    const [isLogin, setIsLogin] = useState(true); // Estado para alternar entre login e registro
+    const [isLogin, setIsLogin] = useState(true);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
+    const [isResetPassword, setIsResetPassword] = useState(false);
     const navigate = useNavigate();
 
     const toggleForm = (e) => {
-        e.preventDefault(); // Previne o comportamento padrão do link
-        setIsLogin(!isLogin); // Alterna entre os formulários
+        e.preventDefault();
+        setIsLogin(!isLogin);
     };
 
     const handleLogin = async (e) => {
@@ -24,49 +25,91 @@ const Login = () => {
             console.log('Login bem-sucedido:', data);
             navigate('/produtos');
         } catch (error) {
-            setError('Nome de usuário ou senha incorretos');
+            setError('E-mail do usuário ou senha incorretos');
         }
     };
 
+    const [resetEmail, setResetEmail] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+
+    const handleResetPassword = (e) => {
+        e.preventDefault();
+        console.log('Resetar senha para:', resetEmail, newPassword);
+        setIsResetPassword(false);
+    };
+
     return (
-        <div className={styles.loginPage}> {/* Usando classes do módulo CSS */}
+        <div className={styles.loginPage}>
             <NavbarComponent />
             <div className={styles.form}>
-                {isLogin ? (
-                    <form className={styles.loginForm} onSubmit={handleLogin}>
-                    <h2 className={styles.welcomeMessage}>Seja Bem-vindo(a)!</h2>
-                    <input
-                        type="text"
-                        placeholder="Nome de usuário"
-                        className={styles.input}
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <input
-                        type="password"
-                        placeholder="Senha"
-                        className={styles.input}
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <button type="submit" className={styles.button} onClick={handleLogin}>login</button>
-                    {error && <p className={styles.error}>{error}</p>}
-                    <p className={styles.message}>
-                        Não tem uma conta? <a href="#" onClick={toggleForm}>Crie uma agora</a>
-                    </p>
-                </form>
-                ) : (
-                    <form className={styles.registerForm}>
-                        <input type="text" placeholder="E-mail" className={styles.input} />
-                        <input type="text" placeholder="Nome de usuário" className={styles.input} />
-                        <input type="password" placeholder="Senha" className={styles.input} />
-                        <input type="text" placeholder="CPF" className={styles.input} />
-                        <input type="text" placeholder="Telefone" className={styles.input} />
-                        <button type="submit" className={styles.button}>criar</button>
+                {isResetPassword ? (
+                    <div className={styles.modalContent}>
+                        <h2 className={styles.welcomeMessage}>Trocar Senha</h2>
+                        <form onSubmit={handleResetPassword}>
+                            <input
+                                type="email"
+                                placeholder="E-mail"
+                                className={styles.input}
+                                value={resetEmail}
+                                onChange={(e) => setResetEmail(e.target.value)}
+                            />
+                            <input
+                                type="password"
+                                placeholder="Nova Senha"
+                                className={styles.input}
+                                value={newPassword}
+                                onChange={(e) => setNewPassword(e.target.value)}
+                            />
+                            <button type="submit" className={styles.button}>Resetar Senha</button>
+                        </form>
                         <p className={styles.message}>
-                            Já tem uma conta? <a href="#" onClick={toggleForm}>Login</a>
+                            Lembrou a senha? <a href="#" onClick={() => setIsResetPassword(false)}>Voltar ao Login</a>
                         </p>
-                    </form>
+                    </div>
+                ) : (
+                    isLogin ? (
+                        <form className={styles.loginForm} onSubmit={handleLogin}>
+                            <h2 className={styles.welcomeMessage}>Seja Bem-vindo(a)!</h2>
+                            <input
+                                type="text"
+                                placeholder="E-mail do usuário"
+                                className={styles.input}
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <input
+                                type="password"
+                                placeholder="Senha"
+                                className={styles.input}
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <button type="submit" className={styles.button} onClick={handleLogin}>login</button>
+                            {error && <p className={styles.error}>{error}</p>}
+                            <p className={styles.message}>
+                                Não tem uma conta? <a href="#" onClick={toggleForm}>Crie uma agora</a>
+                            </p>
+                            <p className={styles.message}>
+                                Esqueceu a senha? <a href="#" onClick={() => setIsResetPassword(true)}>Trocar senha</a>
+                            </p>
+                        </form>
+                    ) : (
+                        <div>
+                            <h2 className={styles.welcomeMessage}>Cadastro</h2>
+                            <form className={styles.registerForm}>
+                                <input type="text" placeholder="E-mail" className={styles.input} />
+                                <input type="text" placeholder="Nome de usuário" className={styles.input} />
+                                <input type="password" placeholder="Senha" className={styles.input} />
+                                <input type="text" placeholder="CPF" className={styles.input} />
+                                <input type="text" placeholder="Telefone" className={styles.input} />
+                                <button type="submit" className={styles.button}>criar</button>
+                                <p className={styles.message}>
+                                    Já tem uma conta? <a href="#" onClick={toggleForm}>Login</a>
+                                </p>
+                            </form>
+                        </div>
+                    
+                    )
                 )}
             </div>
             <Footer />
