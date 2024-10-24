8cdaf6efc97784eb29892144ef56903c63ed5967
GeoFernandes <geovannaferoliveira@gmail.com>
2024-10-24 18:59:27 -0300

    Rollback

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'; // Importando useState
 import { login } from '../../services/loginService';
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom';
 import Footer from '../footer'; // Importando o componente Footer
 import NavbarComponent from '../navbar'; // Importando o componente Navbar
 import styles from './login.module.css'; // Importando o CSS Module
@@ -33,40 +33,40 @@ const Login = () => {
             <NavbarComponent />
             <div className={styles.form}>
                 {isLogin ? (
-                    <form className={styles.loginForm} onSubmit={handleLogin}>
-                    <h2 className={styles.welcomeMessage}>Seja Bem-vindo(a)!</h2>
-                    <input
-                        type="text"
+                        <form className={styles.loginForm} onSubmit={handleLogin}>
+                            <h2 className={styles.welcomeMessage}>Seja Bem-vindo(a)!</h2>
+                            <input
+                                type="text"
                         placeholder="Nome de usuário"
-                        className={styles.input}
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <input
-                        type="password"
-                        placeholder="Senha"
-                        className={styles.input}
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <button type="submit" className={styles.button} onClick={handleLogin}>login</button>
-                    {error && <p className={styles.error}>{error}</p>}
-                    <p className={styles.message}>
-                        Não tem uma conta? <a href="#" onClick={toggleForm}>Crie uma agora</a>
-                    </p>
-                </form>
-                ) : (
-                    <form className={styles.registerForm}>
-                        <input type="text" placeholder="E-mail" className={styles.input} />
-                        <input type="text" placeholder="Nome de usuário" className={styles.input} />
-                        <input type="password" placeholder="Senha" className={styles.input} />
-                        <input type="text" placeholder="CPF" className={styles.input} />
-                        <input type="text" placeholder="Telefone" className={styles.input} />
-                        <button type="submit" className={styles.button}>criar</button>
-                        <p className={styles.message}>
-                            Já tem uma conta? <a href="#" onClick={toggleForm}>Login</a>
-                        </p>
-                    </form>
+                                className={styles.input}
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <input
+                                type="password"
+                                placeholder="Senha"
+                                className={styles.input}
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <button type="submit" className={styles.button} onClick={handleLogin}>login</button>
+                            {error && <p className={styles.error}>{error}</p>}
+                            <p className={styles.message}>
+                                Não tem uma conta? <a href="#" onClick={toggleForm}>Crie uma agora</a>
+                            </p>
+                        </form>
+                    ) : (
+                            <form className={styles.registerForm}>
+                                <input type="text" placeholder="E-mail" className={styles.input} />
+                                <input type="text" placeholder="Nome de usuário" className={styles.input} />
+                                <input type="password" placeholder="Senha" className={styles.input} />
+                                <input type="text" placeholder="CPF" className={styles.input} />
+                                <input type="text" placeholder="Telefone" className={styles.input} />
+                                <button type="submit" className={styles.button}>criar</button>
+                                <p className={styles.message}>
+                                    Já tem uma conta? <a href="#" onClick={toggleForm}>Login</a>
+                                </p>
+                            </form>
                 )}
             </div>
             <Footer />
