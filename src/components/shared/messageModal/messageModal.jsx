08583ea94accd26b145defbbe00ce51d05b9927f
GeoFernandes <geovannaferoliveira@gmail.com>
2025-02-15 15:21:35 -0300

    Modal de mensagem generica

---
+++ src/components/shared/messageModal/messageModal.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './messageModal.module.css';
+
+// eslint-disable-next-line react/prop-types
+const MessageModal = ({ icon, message, onClose }) => {
+    return (
+        <div className={styles.modalOverlay}>
+            <div className={styles.modalContent}>
+                <button className={styles.closeButton} onClick={onClose}>
+                    &times;
+                </button>
+                <div className={styles.iconContainer}>
+                    {icon}
+                </div>
+                <div className={styles.messageContainer}>
+                    <p>{message}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MessageModal;
