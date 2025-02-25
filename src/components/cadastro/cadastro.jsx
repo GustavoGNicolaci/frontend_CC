3373e7bfe7086d2c97425c35781e34e3d3be8e68
GeoFernandes <geovannaferoliveira@gmail.com>
2025-02-25 19:48:14 -0300

    Novos campos para a tela de cadastro

--- src/components/cadastro/cadastro.jsx
+++ src/components/cadastro/cadastro.jsx
@@ -1,4 +1,3 @@
-// Cadastro.jsx
 import React, { useState } from 'react';
 import InputMask from 'react-input-mask';
 import { registro } from '../../services/loginService';
@@ -18,6 +17,11 @@ const Cadastro = () => {
     const [confirmPassword, setConfirmPassword] = useState('');
     const [cpf, setCpf] = useState('');
     const [phone, setPhone] = useState('');
+    const [cep, setCep] = useState('');
+    const [rua, setRua] = useState('');
+    const [estado, setEstado] = useState('');
+    const [cidade, setCidade] = useState('');
+    const [bairro, setBairro] = useState('');
     const [fieldErrors] = useState({});
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
@@ -28,7 +32,7 @@ const Cadastro = () => {
         setIsLoading(true);
         setErrorMessage('');
         try {
-            await registro(email, name, password, cpf, phone, confirmPassword);
+            await registro(email, name, password, cpf, phone, confirmPassword, cep, rua, estado, cidade, bairro);
             setIsModalOpen(true); 
         } catch (error) {
             setErrorMessage('Erro ao fazer registro. Por favor, tente novamente.');
@@ -40,26 +44,16 @@ const Cadastro = () => {
     const closeModal = () => {
         setIsModalOpen(false);
         navigate('/login');
-
     };
 
     const toggleForm = () => {};
 
     return (
-        <div>
+        <div className={styles.container}>
             <h2 className={styles.welcomeMessage}>Cadastre-se</h2>
             <form className={styles.registerForm} onSubmit={handleSubmit}>
                 <div className={styles.informacoesPessoais}>
-                    <input
-                        type="email"
-                        placeholder="E-mail"
-                        className={`${styles.input} ${fieldErrors.email ? styles.errorInput : ''}`}
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                    {fieldErrors.email && <p className={styles.error}>{fieldErrors.email}</p>}
-                    
+
                     <input
                         type="text"
                         placeholder="Nome"
@@ -77,6 +71,16 @@ const Cadastro = () => {
                         {(inputProps) => <input {...inputProps} type="text" placeholder="CPF" className={styles.input} />}
                     </InputMask>
                     {fieldErrors.cpf && <p className={styles.error}>{fieldErrors.cpf}</p>}
+
+                    <input
+                        type="email"
+                        placeholder="E-mail"
+                        className={`${styles.input} ${fieldErrors.email ? styles.errorInput : ''}`}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                    {fieldErrors.email && <p className={styles.error}>{fieldErrors.email}</p>}
                             
                     <InputMask
                         mask="(99) 99999-9999"
@@ -86,6 +90,53 @@ const Cadastro = () => {
                         {(inputProps) => <input {...inputProps} type="text" placeholder="Telefone" className={styles.input} />}
                     </InputMask>
                     {fieldErrors.phone && <p className={styles.error}>{fieldErrors.phone}</p>}
+                </div>
+
+                <div className={styles.endereco}>
+                    <InputMask
+                        mask="99999-999"
+                        value={cep}
+                        onChange={(e) => setCep(e.target.value)}
+                    >
+                        {(inputProps) => <input {...inputProps} type="text" placeholder="CEP" className={styles.input} />}
+                    </InputMask>
+                    {fieldErrors.cep && <p className={styles.error}>{fieldErrors.cep}</p>}
+
+                    <input
+                        type="text"
+                        placeholder="Rua"
+                        className={styles.input}
+                        value={rua}
+                        onChange={(e) => setRua(e.target.value)}
+                    />
+                    {fieldErrors.rua && <p className={styles.error}>{fieldErrors.rua}</p>}
+
+                    <input
+                        type="text"
+                        placeholder="Estado"
+                        className={styles.input}
+                        value={estado}
+                        onChange={(e) => setEstado(e.target.value)}
+                    />
+                    {fieldErrors.estado && <p className={styles.error}>{fieldErrors.estado}</p>}
+
+                    <input
+                        type="text"
+                        placeholder="Cidade"
+                        className={styles.input}
+                        value={cidade}
+                        onChange={(e) => setCidade(e.target.value)}
+                    />
+                    {fieldErrors.cidade && <p className={styles.error}>{fieldErrors.cidade}</p>}
+
+                    <input
+                        type="text"
+                        placeholder="Bairro"
+                        className={styles.input}
+                        value={bairro}
+                        onChange={(e) => setBairro(e.target.value)}
+                    />
+                    {fieldErrors.bairro && <p className={styles.error}>{fieldErrors.bairro}</p>}
                 </div>
 
                 <div className={styles.confirmarSenha}>
@@ -108,11 +159,11 @@ const Cadastro = () => {
                     {fieldErrors.confirmPassword && <p className={styles.error}>{fieldErrors.confirmPassword}</p>}
                 </div>
 
-                <button type="submit" className={styles.button}>Cadastrar</button>
-                <p className={styles.message}>
-                    Já tem uma conta? <a href="/login" className={styles.link} onClick={toggleForm}>Login</a>
-                </p>
             </form>
+            <button type="submit" className={styles.button}>Cadastrar</button>
+            <p className={styles.message}>
+                Já tem uma conta? <a href="/login" className={styles.link} onClick={toggleForm}>Login</a>
+            </p>
 
             {isLoading && <LoadingModal />}
 
