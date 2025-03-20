dac7a2193dbe139a4225a455ceaf2673ff528134
GeoFernandes <geovannaferoliveira@gmail.com>
2025-03-20 12:52:48 -0300

    Adição de contexto para autenticação e implementação das rotas privadas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import PaginaInicial from './components/paginaInicial/paginaInicial';
-import Login from './components/login/login';
-import Produtos from './components/produtos';
-import DetalhesProduto from './components/detalhesProduto';
+import { Route, Routes } from 'react-router-dom';
+import { CartProvider } from './CartContext';
+import PrivateRoute from './authenticate/privateRoute';
 import Carrinho from './components/carrinho';
 import Checkout from './components/checkout';
-import { CartProvider } from './CartContext';
-import UserProfile from './components/perfil/UserProfile'; 
 import Contato from './components/contato/contato';
+import DetalhesProduto from './components/detalhesProduto';
+import Login from './components/login/login';
 import NossasLojas from './components/nossasLojas/nossasLojas';
-import TrabalheConosco from './components/trabalheConosco/trabalheConosco';
+import PaginaInicial from './components/paginaInicial/paginaInicial';
+import UserProfile from './components/perfil/UserProfile';
+import Produtos from './components/produtos';
 import SobreCafeConnect from './components/sobreCafeConnect/sobreCafeConnect';
+import TrabalheConosco from './components/trabalheConosco/trabalheConosco';
 import './styles/App.css';
 
 function App() {
   return (
     <CartProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<PaginaInicial />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/produtos" element={<Produtos />} />
-          <Route path="/detalhesProduto" element={<DetalhesProduto />} />
+      <Routes>
+        {/* Rotas públicas */}
+        <Route path="/" element={<PaginaInicial />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/produtos" element={<Produtos />} />
+        <Route path="/detalhesProduto" element={<DetalhesProduto />} />
+        <Route path="/contato" element={<Contato />} />
+        <Route path="/nossas-lojas" element={<NossasLojas />} />
+        <Route path="/trabalhe-conosco" element={<TrabalheConosco />} />
+        <Route path="/sobre-cafe-connect" element={<SobreCafeConnect />} />
+
+        {/* Rotas protegidas */}
+        <Route element={<PrivateRoute />}>
           <Route path="/carrinho" element={<Carrinho />} />
           <Route path="/checkout" element={<Checkout />} />
-          <Route path="/perfil" element={<UserProfile />} /> 
-          <Route path="/contato" element={<Contato />} />
-          <Route path="/nossas-lojas" element={<NossasLojas />} /> 
-          <Route path="/trabalhe-conosco" element={<TrabalheConosco />} />
-          <Route path="/sobre-cafe-connect" element={<SobreCafeConnect />} />
-        </Routes>
-      </Router>
+          <Route path="/perfil" element={<UserProfile />} />
+        </Route>
+      </Routes>
     </CartProvider>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { AuthProvider } from './authenticate/authContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <AuthProvider> {/* 🔹 Aqui garante que o contexto está dentro do BrowserRouter */}
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
